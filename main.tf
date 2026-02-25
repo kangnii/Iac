56ac51404e340ba2389e8af0c5b7e5ff7b1fedf6
kangnii <wisdomfollygan@gmail.com>
2026-02-25 14:31:42 +0100

    First commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.54"
+    }
+  }
+}
+ 
+# Utilise clouds.yaml pour GRA9
+provider "openstack" {
+  cloud = "ovh-gra9"
+}
+ 
+resource "openstack_compute_keypair_v2" "main" {
+  name       = "my-keypair-wisdom"
+  public_key = file("/home/wisdom-follygan/Téléchargements/ssh-key-2026-02-19.key.pub")
+}
+ 
+resource "openstack_compute_instance_v2" "main" {
+  name            = "iac-wisdom-vm"
+  image_name      = "Ubuntu 24.04"
+  flavor_name     = "d2-2"
+  key_pair        = openstack_compute_keypair_v2.main.name
+  security_groups = ["default"]
+ 
+  network {
+    name = "Ext-Net"
+  }
+}
