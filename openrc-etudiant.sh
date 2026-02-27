1a40a95345585c7d49e11782e76a2a5bc274a797
kangnii <wisdomfollygan@gmail.com>
2026-02-27 12:26:47 +0100

    Third pipe

--- openrc-etudiant.sh
+++ openrc-etudiant.sh
@@ -8,6 +8,9 @@
 export OS_AUTH_URL=https://auth.cloud.ovh.net/v3
 export OS_IDENTITY_API_VERSION=3
 
+export OS_USER_DOMAIN_NAME="Default"
+export OS_PROJECT_DOMAIN_NAME="Default"
+
 #export OS_USER_DOMAIN_NAME=${OS_USER_DOMAIN_NAME:-"Default"}
 #export OS_PROJECT_DOMAIN_NAME=${OS_PROJECT_DOMAIN_NAME:-"Default"}
 
@@ -27,6 +30,7 @@ export OS_PASSWORD=S9r53uYZcTYSwHQxErSS5bbXdAgpzbRx
 # If your configuration has multiple regions, we set that information here.
 # OS_REGION_NAME is optional and only valid in certain environments.
 export OS_REGION_NAME="GRA9"
+export OS
 
 export AWS_ACCESS_KEY_ID="ee1b599fe4054f1c87254b2ea1126724"
 export AWS_SECRET_ACCESS_KEY="86a09bc6ce3f43829b0c149e5f929aa5"
